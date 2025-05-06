a74dcb8a40b27c42e3c66c5fdd134b244a710a58
SandeepBiswalG <71204243+iSandeepBiswalG@users.noreply.github.com>
2025-05-06 20:06:29 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(e) {
+  console.log('Service Worker Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('Service Worker Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
